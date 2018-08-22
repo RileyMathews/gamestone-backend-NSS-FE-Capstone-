39719660d1bbbbeecdcac403a063969acacf2fa4
Riley Mathews <rileymathews80@gmail.com>
2018-08-21 22:18:23 -0500

    started filtering user

--- api/views/user_view.py
+++ api/views/user_view.py
@@ -4,5 +4,17 @@ from api.serializers import UserSerializer
 
 class UserViewset(viewsets.ModelViewSet):
     """ viewset for the user model """
-    queryset = User.objects.all()
+    # queryset = User.objects.all()
     serializer_class = UserSerializer
+    def get_queryset(self):
+        """ method to control the query of the users table
+            The view should filter by active users if the url contains the filter
+        """
+        queryset = models.User.objects.all()
+        query_string = self.request.query_params.get('gamertag', None)
+
+        # take the string value from the url and set a boolean value accordingly
+        if query_string is not None:
+            queryset = queryset.filter(gamertag=query_string)
+
+        return queryset
