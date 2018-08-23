f829a2a496b3e7b99b24b567fd7d7fd2e810b0f2
Riley Mathews <rileymathews80@gmail.com>
2018-08-23 17:38:55 -0500

    added models to admin site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Platform, User, UserGame, UserPlatform
 # Register your models here.
+
+
+admin.site.register(Platform)
+admin.site.register(User)
+admin.site.register(UserGame)
+admin.site.register(UserPlatform)
