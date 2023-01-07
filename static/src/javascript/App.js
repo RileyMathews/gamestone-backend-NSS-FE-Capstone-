896e7a3deb6ad44a8f0086c52c1be360d338433a
Riley Mathews <rileymathews80@gmail.com>
2023-01-07 11:21:49 -0600

    navbar now works on mobile

--- static/src/javascript/App.js
+++ static/src/javascript/App.js
@@ -11,7 +11,6 @@ class App extends Component {
     render() {
         return (
             <div>
-                <br></br>
                 <Provider>
                     <Context.Consumer>
                         {context => (
--- static/src/javascript/nav/NavBar.js
+++ static/src/javascript/nav/NavBar.js
@@ -12,23 +12,7 @@ class NavBar extends Component {
     // Storing session storage as an object in state named currentUser
     state = {
         isActive: false,
-        firstName: "",
         searchType: "All"
-    }
-
-    // Making a fetch request against sessionStorage to find relevant user and storing first name in state
-    componentDidMount() {
-        const currentUser = sessionStorage.getItem('userId')
-        if (currentUser !== null) {
-            fetch(`${url}user/${currentUser}`)
-                .then(r => r.json())
-                .then(response => {
-                    this.setState({
-                        firstName: response.first_name,
-                        image: response.image
-                    })
-                })
-        }
     }
 
     // event handler for clicking nav drop down burger
@@ -52,21 +36,19 @@ class NavBar extends Component {
 
     render() {
         return (
-            <Navbar className="is-fixed-top">
+            <Navbar active={this.state.isActive}>
                 <Navbar.Brand>
                     <img className="logo" alt="logo" src="/static/images/logo.png" />
-                    <Navbar.Item>{this.props.gamertag}</Navbar.Item>
                     <Navbar.Burger onClick={this.onClickNav} />
                 </Navbar.Brand>
                 <Navbar.Menu>
-                    <Navbar.Item active={this.getCurrentView("profile")} className="clickable" id="nav__profile" onClick={this.onClickNav}>My Profile</Navbar.Item>
-                    <Navbar.Item active={this.getCurrentView("search")} className="clickable" id="nav__search" onClick={this.onClickNav}>Add Games</Navbar.Item>
-                    <Navbar.Item active={this.getCurrentView("suggest")} className="clickable" id="nav__suggest" onClick={this.onClickNav}>Suggest Games</Navbar.Item>
-                    <Navbar.Item active={this.getCurrentView("logout")} className="clickable" id="nav__logout" onClick={this.onClickNav}>Logout</Navbar.Item>
+                    <Navbar.Container>
+                        <Navbar.Item className="clickable" id="nav__profile" onClick={this.onClickNav}>My Profile</Navbar.Item>
+                        <Navbar.Item className="clickable" id="nav__search" onClick={this.onClickNav}>Add Games</Navbar.Item>
+                        <Navbar.Item className="clickable" id="nav__suggest" onClick={this.onClickNav}>Suggest Games</Navbar.Item>
+                        <Navbar.Item className="clickable" id="nav__logout" onClick={this.onClickNav}>Logout</Navbar.Item>
+                    </Navbar.Container>
                 </Navbar.Menu>
-                <Navbar.Container align='right'>
-                    <Navbar.Item>{this.props.gamertag}</Navbar.Item>
-                </Navbar.Container>
             </Navbar>
         )
 
