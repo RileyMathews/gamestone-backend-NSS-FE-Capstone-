973f31833dd32b1925d4334cc3108d37476a09db
Riley Mathews <rileymathews80@gmail.com>
2023-01-23 23:06:35 -0600

    updated for new fly app

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,10 +3,10 @@ from .base import get_env_variable
 import dj_database_url
 
 DEBUG = False
-ALLOWED_HOSTS = ["gamestone.fly.dev"]
+ALLOWED_HOSTS = ["rm-django-demos.fly.dev"]
 DATABASES = {"default": dj_database_url.parse(get_env_variable("DATABASE_URL"))}
 SECRET_KEY = get_env_variable("SECRET_KEY")
-CSRF_COOKIE_DOMAIN = "gamestone.fly.dev"
-CSRF_TRUSTED_ORIGINS = ["https://gamestone.fly.dev"]
+CSRF_COOKIE_DOMAIN = "rm-django-demos.fly.dev"
+CSRF_TRUSTED_ORIGINS = ["https://rm-django-demos.fly.dev"]
 
 GIANTBOMB_API_KEY = get_env_variable("GIANTBOMB_API_KEY")
