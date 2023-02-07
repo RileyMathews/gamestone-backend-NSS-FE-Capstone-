04a7d7eeacc68a876215ffa7cfdfdbf14321e668
Riley Mathews <rileymathews80@gmail.com>
2023-02-07 13:55:12 -0600

    reset app

--- resource_tracker/urls.py
+++ resource_tracker/urls.py
@@ -5,15 +5,7 @@ from . import views
 
 router = DefaultRouter()
 
-router.register("players", viewsets.PlayerViewSet)
-router.register("resources", viewsets.ResourceViewSet)
 
 urlpatterns = [
-    path("api/", include(router.urls)),
-    path("", views.player_list, name="player_list"),
-    path("players/create/", views.player_create, name="player_create"),
-    path("players/<uuid:pk>/", views.player_detail, name="player_detail"),
-    path("players/<uuid:pk>/confirm-delete/", views.player_delete, name="player_delete"),
-    path("players/<uuid:player_id>/add-resource/", views.resource_create, name="resource_create"),
-    path("resources/<uuid:pk>/delete", views.resource_delete, name="resource_delete")
+
 ]
