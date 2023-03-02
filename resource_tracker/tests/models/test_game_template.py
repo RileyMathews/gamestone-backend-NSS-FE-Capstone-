b95c9029f81bb9c3a3256cde107101418351be31
Riley Mathews <rileymathews80@gmail.com>
2023-03-02 11:25:11 -0600

    fixed tests

--- resource_tracker/tests/models/test_game_template.py
+++ resource_tracker/tests/models/test_game_template.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
 from resource_tracker.models import GameTemplate, Player
 from django.contrib.auth import get_user_model
+from uuid import uuid4
 
 class GameTemplateTestCase(TestCase):
 
     def setUp(self):
-        user = get_user_model().objects.create()
+        user = get_user_model().objects.create_user(uuid4())
         self.player = Player.objects.create(user=user, name="test")
 
     def test_can_create_game_template(self):
@@ -17,4 +18,3 @@ class GameTemplateTestCase(TestCase):
         )
 
         self.assertEqual(game_template.name, db_game_template.name)
-
--- resource_tracker/tests/models/test_player.py
+++ resource_tracker/tests/models/test_player.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
 from resource_tracker.models import Player
 from django.contrib.auth import get_user_model
+from uuid import uuid4
 
 
 class PlayerTestCase(TestCase):
     def test_can_create_player(self):
-        user = get_user_model().objects.create()
+        user = get_user_model().objects.create_user(uuid4())
         player = Player.objects.create(user=user, name="test name")
         db_player = Player.objects.get(id=player.id)
 
